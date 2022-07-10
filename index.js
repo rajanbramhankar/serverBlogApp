e379de5f8447eeb1b6816bd4abae70ba559f36ad
rajanbramhankar <rajanbramhankar66@gmail.com>
2022-07-10 15:38:30 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
  const express=require('express')
 // const detailsRouter2= require('./routes')
+const cors = require('cors')
 const detailsRouter1 = require('./routes/details')
  const app = express()
-
+app.use(cors())
  app.use("/api",detailsRouter1)
 
- app.listen(3001,()=>{
+ app.listen(  process.env.PORT || 3001,()=>{
     console.log("app is running");
  })
